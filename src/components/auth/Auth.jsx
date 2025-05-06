82f241d122a181bf0b4e9077ef3d104dc184e1d1
nhlamulomagwaza <magwazanhlamulo563@gmail.com>
2025-05-06 06:00:22 +0200

    updated admin dash

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -5,8 +5,8 @@ import { useEffect } from 'react';
 import ScaleLoader from "react-spinners/ScaleLoader";
 const Auth = () => {
 
-   const [email, setEmail]= useState('');
-   const [password, setPassword]= useState('');
+   const [email, setEmail]= useState('info@ninefiverecruitment.com');
+   const [password, setPassword]= useState('^mgaKTIhNl96Q');
    const [validationErrors, setValidationErrors]= useState(false);
    const [emailValidateErrors, setEmailValidateErrors] = useState(false);
   const [passwordValidateErrors, setPasswordValidateErrors] = useState(false);
@@ -186,13 +186,20 @@ const Auth = () => {
     <form action="" className="sign-in" onSubmit={handleSignIn}>
     {/*   <h1 className="auth-title">Sign In</h1> */}
         <p className="signin-cta">Please sign in with your credentials to continue</p>
-      <input type="text" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}
+      <input type="text" placeholder='email' value={email.trim()} onChange={(e) => setEmail(e.target.value)}
       style={emailValidateErrors ? { backgroundColor: '#ffaea8', border: '1px solid #ff2626' } : { backgroundColor: 'white' }} />
-      <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}
+      <input type="password" placeholder='password' value={password.trim()} onChange={(e) => setPassword(e.target.value)}
       style={passwordValidateErrors ? { backgroundColor: '#ffaea8', border: '1px solid #ff2626' } : { backgroundColor: 'white' }}
 
       />
       <button className="sign-btn">{loading ? <ScaleLoader className='loader' color='white' height={10} /> : 'Sign In'}</button>
+      <div className="demo-logins">
+        <p className="demo-login">Demo Login</p>
+        <div className="demo-credentials">
+          <p className="demo-email">Email: info@ninefiverecruitment.com</p>
+          <p className="demo-password">Password: ^mgaKTIhNl96Q</p>
+      </div>
+      </div>
       </form>
    </section>
   )
